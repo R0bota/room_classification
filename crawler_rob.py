e3ea47629cb96e254f8d9e2faec6db7bb932cc44
Robert Hager <RobertHager@gmx.de>
2020-11-27 23:33:35 +0100

    new gitignore

--- crawler_rob.py
+++ crawler_rob.py
@@ -1,77 +1,34 @@
+import requests
 import bs4 as bs
 import urllib.request
-import time
-from datetime import datetime
-import pandas as pd
-import json
+import os
 
+url = str(input('URL: '))
 
-df = pd.DataFrame()
+opener = urllib.request.build_opener()
+opener.add_headers = [{'User-Agent': 'Mozilla'}]
+urllib.request.install_opener(opener)
 
-for seite in range(1,2):
-    
-    print("Loop " + str(seite) + " startet.")
-    
-    l=[]
+raw = requests.get(url).text
+soup = bs.BeautifulSoup(raw, 'html.parser')
 
-    try:
-        
-        print("https://www.immobilienscout24.de/Suche/S-2/P-" + str(seite) + "/Wohnung-Kauf")
-        soup = bs.BeautifulSoup(urllib.request.urlopen("https://www.immowelt.de/liste/muenchen-altstadt-lehel/wohnungen/mieten?geoid=10809162000017&zip=80333&sort=relevanz").read(), 'lxml')
-        print("Aktuelle Seite: "+"https://www.immobilienscout24.de/Suche/S-2/P-" + str(seite) + "/Wohnung-Kauf/archiv")
-        
-        for paragraph in soup.find_all("a"):
+meta = soup.find_all('meta')
 
-            if r"/expose/" in str(paragraph.get("href")):
-                l.append(paragraph.get("href").split("#")[0])
+print(meta)
 
-            l = list(set(l))
+""" links = []
 
-        for item in l:
+for img in imgs:
+    link = img.get('src')
+    if 'http://' not in link:
+        link = url + link
+    links.append(link)
 
-            try:
+print(links[0])
+print('Anzahl Bilder auf Seite: ' + str(len(links))) """
 
-                soup = bs.BeautifulSoup(urllib.request.urlopen('https://www.immobilienscout24.de'+item).read(),'lxml')
-
-                data = pd.DataFrame(json.loads(str(soup.find_all("script")).split("keyValues = ")[1].split("}")[0]+str("}")),index=[str(datetime.now())])
-
-                data["URL"] = str(item)
-                print(str(item))
-                beschreibung = []
-				
-				
-	
-			
-
-                for x in soup.find_all("pre"):
-                    beschreibung.append(x.text)
-                    
-                	
-
-                #data["beschreibung"] = str(beschreibung)
-                data["beschreibung"] = str()
-                for i in soup.find_all("dd", attrs={"class":"is24qa-heizkosten grid-item three-fifths"}):
-                    beschreibung = i.text
-                    beschreibung = beschreibung.replace("\n", " ")	
-                    beschreibung = beschreibung.replace("\t", " ")		
-                    beschreibung = beschreibung.replace(";", " ")		    			
-                #data["HK enthalten"] = str(beschreibung)
-                data["HK enthalten"] = str()
-                beschreibung = ""
-                data["obj_street"] = str()
-                df = df.append(data)
-
-            except Exception as e: 
-                print(str(datetime.now())+": " + str(e))
-                l = list(filter(lambda x: x != item, l))
-                print("ID " + str(item) + " entfernt.")
-     
-        
-        print("Loop " + str(seite) + " endet.\n")
-        
-    except Exception as e: 
-        print(str(datetime.now())+": " + str(e))
-
-print("FERTIG!")
-
-#df.to_csv("Wohnung_Kauf-" + str( datetime.now())[:19].replace(":","").replace(".","").replace("-",".") + ".csv",sep=";",decimal=",",encoding = "utf-8",index_label="timestamp")  
+""" for i in range(len(links)):
+    filename = 'data\\out\\img{}.jpg'.format(i)
+    urllib.request.urlretrieve(links[i], filename)
+    print('Fertig')
+ """
