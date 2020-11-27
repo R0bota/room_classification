a0483e08154bd2523c301945a9cd053c16a8c4c9
joreka27 <j_resch@web.de>
2020-11-27 23:26:43 +0100

    Image Crawler

--- jo_test.py
+++ jo_test.py
@@ -1,7 +1,7 @@
 import requests
 import bs4 as bs
 import urllib.request
-import os
+
 
 url = str(input('URL: '))
 
@@ -12,15 +12,17 @@ urllib.request.install_opener(opener)
 raw = requests.get(url).text
 soup = bs.BeautifulSoup(raw, 'html.parser')
 
-imgs = soup.find_all('img')
+metas = soup.find_all('meta')
 
 links = []
 
-for img in imgs:
-    link = img.get('src')
-    if 'http://' not in link:
-        link = url + link
-    links.append(link)
+for meta in metas:
+    prop = meta.get('property')
+    content = meta.get('content')
+    print('Property: ' + str(prop))
+    if str(prop) == 'og:image':
+        print('Property: ' + str(content))
+        links.append(str(content))
 
 print('Anzahl Bilder auf Seite: ' + str(len(links)))
 
@@ -28,3 +30,24 @@ for i in range(len(links)):
     filename = 'data\\out\\img{}.png'.format(i)
     urllib.request.urlretrieve(links[i], filename)
     print('Fertig')
+
+
+# raw = requests.get(url).text
+# soup = bs.BeautifulSoup(raw, 'html.parser')
+
+# imgs = soup.find_all('img')
+
+# links = []
+
+# for img in imgs:
+#     link = img.get('src')
+#     if 'http://' not in link:
+#         link = url + link
+#     links.append(link)
+
+# print('Anzahl Bilder auf Seite: ' + str(len(links)))
+
+# for i in range(len(links)):
+#     filename = 'data\\out\\img{}.png'.format(i)
+#     urllib.request.urlretrieve(links[i], filename)
+#     print('Fertig')
