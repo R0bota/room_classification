ec0c184fe96c73f1152f61e4086368383ffd37b1
joreka27 <j_resch@web.de>
2020-11-28 00:59:55 +0100

    Crawl several

--- jo_test.py
+++ jo_test.py
@@ -2,48 +2,58 @@ import requests
 import bs4 as bs
 import urllib.request
 
-
-url = str(input('URL: '))
+# url = str(input('URL: '))
+url = 'https://www.immowelt.de/liste/muenchen-altstadt-lehel/wohnungen/mieten?geoid=10809162000017&prima=900&sort=createdate%2Bdesc'
 
 opener = urllib.request.build_opener()
 opener.add_headers = [{'User-Agent': 'Mozilla'}]
 urllib.request.install_opener(opener)
 
-raw = requests.get(url).text
-soup = bs.BeautifulSoup(raw, 'html.parser')
+rawMain = requests.get(url).text
+soup = bs.BeautifulSoup(rawMain, 'html.parser')
 
-metas = soup.find_all('meta')
+# metas = soup.find_all('meta')
+div_tags = soup.find_all('div')
+oIds = []
+for div in div_tags:
+    oId = div.get('data-oid')
+    if oId is not None:
+        print(str(oId))
+        oidUrl = 'https://www.immowelt.de/expose/' + str(oId)
+        raw = requests.get(oidUrl).text
+        soupOid = bs.BeautifulSoup(raw, 'html.parser')
+        metas = soupOid.find_all('meta')
 
-links = []
+        links = []
 
-for meta in metas:
-    prop = meta.get('property')
-    content = meta.get('content')
-    print('Property: ' + str(prop))
-    if str(prop) == 'og:image':
-        print('Property: ' + str(content))
-        links.append(str(content))
+        for meta in metas:
+            prop = meta.get('property')
+            content = meta.get('content')
+            if str(prop) == 'og:image':
+                print('Property: ' + str(content))
+                imageUrl = str(content)
+                if 'https:' in imageUrl:
+                    links.append(str(content))
 
-print('Anzahl Bilder auf Seite: ' + str(len(links)))
+        print('Anzahl Bilder auf Seite: ' + str(len(links)))
 
-for i in range(len(links)):
-    filename = 'data\\out\\img{}.png'.format(i)
-    urllib.request.urlretrieve(links[i], filename)
-    print('Fertig')
+        for i in range(len(links)):
+            filenamePart1 = 'data\\out\\' + oId
+            filenamePart2 = '_{}.png'.format(i)
+            filename = filenamePart1 + filenamePart2
+            print(filename)
+            urllib.request.urlretrieve(links[i], filename)
 
-
-# raw = requests.get(url).text
-# soup = bs.BeautifulSoup(raw, 'html.parser')
-
-# imgs = soup.find_all('img')
 
 # links = []
 
-# for img in imgs:
-#     link = img.get('src')
-#     if 'http://' not in link:
-#         link = url + link
-#     links.append(link)
+# for meta in metas:
+#     prop = meta.get('property')
+#     content = meta.get('content')
+#     print('Property: ' + str(prop))
+#     if str(prop) == 'og:image':
+#         print('Property: ' + str(content))
+#         links.append(str(content))
 
 # print('Anzahl Bilder auf Seite: ' + str(len(links)))
 
