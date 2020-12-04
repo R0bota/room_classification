83175a640a2f8e207301b2f17e59812efd1c662a
Robert Hager <RobertHager@gmx.de>
2020-12-04 22:46:34 +0100

    quality check script with plots and stats

--- quality_check.py
+++ quality_check.py
@@ -1,6 +1,7 @@
 ######################################################
 # score images which are already classified to check
-# model quality
+# model quality and plot certain plots to assess
+# accuracy of classification
 ######################################################
 
 # load packages
@@ -12,12 +13,16 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import pandas as pd
+import plotly.express as px
+import seaborn as sns
+import matplotlib as mpl
 from keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator 
 from keras.applications.vgg16 import decode_predictions
 from itertools import chain
 from datetime import datetime
 from sys import platform
+from bokeh.plotting import output_file, show
 
 #check OS
 print("Checking OS...")
@@ -36,36 +41,81 @@ def listfiles(p):
 # specify directory paths
 data_dir = 'data\\'
 model_dir = 'model\\'
-model_id = '2020-11-29_17-49-28_265344'
+model_id = '2020-11-29_12-46-54_144487'
 
+# specify which category to check with which threshold of confidence
+pic_cat = 'sleeping_room'
+th = 0.9
+
+#load model
 model = keras.models.load_model(os.path.join(model_dir, model_id))
-
 dbfile = open(model_dir + model_id + '.pkl', 'rb')      
 labels = pickle.load(dbfile) 
 dbfile.close()
 
-pics = (glob.glob(data_dir + 'classified\\floor_plan' + sep + '*.png'))
+# read files to score
+pics = (glob.glob(data_dir + 'classified\\' + pic_cat + sep + '*.png'))
 
+# variable setup for model results
 sc = []
 cat = []
+pic_id = []
 
 for pic in pics:
 
-    print(pic)
+    # which image is currently scored
+    print(pic[(len(data_dir + 'classified\\' + pic_cat + sep)):(len(pic)-4)])
 
+    # preprocess image
     img = tf.keras.preprocessing.image.load_img(pic, target_size=(224, 224))
     imgs = np.asarray(img)
     img = np.expand_dims(imgs, axis = 0)
 
+    # score image
     output = model.predict(img, batch_size = 1)
     predicted_class_indices = np.argmax(output, axis = 1)
     predictions = [labels[k] for k in predicted_class_indices]
+
+    # only classify if over specified threshold
+    if output[0][predicted_class_indices] < th:
+        predictions[0] = 'unsure'
     
+    # file arrays
     sc.append(output[0][predicted_class_indices])
     cat.append(predictions[0])
+    pic_id.append(pic[(len(data_dir + 'classified\\' + pic_cat + sep)):(len(pic)-4)])
 
-res = pd.DataFrame(sc)
-res['category'] = pd.DataFrame(cat)
-res.columns = ['score', 'category']
-print(res.score.describe())
-print(res.category.value_counts())
+# create result dataframe
+model_res = pd.DataFrame(pic_id)
+model_res['predicted_category'] = pd.DataFrame(cat)
+model_res['true_category'] = pic_cat
+model_res['score'] = pd.DataFrame(sc)
+model_res.columns = ['pic_id', 'predicted_category', 'true_category', 'score']
+
+# save results to csv for jupyter
+#res.to_csv(data_dir + 'sample_results.csv')
+
+# check df
+print(model_res.head())
+print(model_res.score.describe())
+print(model_res.predicted_category.value_counts())
+
+# plotly theme
+template = 'plotly_dark' 
+
+# confusion matrix
+confusion_matrix = pd.crosstab(model_res['true_category'], model_res['predicted_category'], 
+                               rownames = ['Actual'], colnames = ['Predicted'], margins = False)
+
+sns.heatmap(confusion_matrix, annot = True, cbar = False)
+plt.show()
+
+# plot score distribution
+fig = px.violin(model_res, y = "score", x = "true_category", box = True, points = "all",
+          hover_data = ['pic_id'], template = template)
+fig.show()
+
+# plot score distribution over different categories plotly
+fig = px.violin(model_res, y = "score", x = "predicted_category", color = "predicted_category", 
+                box = True, points = "all", hover_data = ['pic_id'], template = template)
+fig.show()
